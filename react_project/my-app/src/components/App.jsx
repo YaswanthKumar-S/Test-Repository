a8bd67eb1d555d470735765c9f9c05738660d3c9
YaswanthKumar-S <118246405+YaswanthKumar-S@users.noreply.github.com>
2024-07-24 10:08:05 +0000

    Changes to be committed: 	deleted:    react_project/my-app/src/App.css 	deleted:    react_project/my-app/src/App.js 	modified:   react_project/my-app/src/App.test.js 	new file:   react_project/my-app/src/assets/css/App.css 	renamed:    react_project/my-app/src/index.css -> react_project/my-app/src/assets/css/index.css 	new file:   react_project/my-app/src/components/App.jsx 	new file:   react_project/my-app/src/components/login.jsx 	modified:   react_project/my-app/src/index.js

---
+++ react_project/my-app/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "./login";
+function App(){
+  return (
+    <>
+    <h1 style={{textAlign:'center'}}>Hello World!</h1>
+    <Login></Login>
+    </>
+  );
+}
+export default App;
